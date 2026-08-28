d31496ff65186d3d482877329f11e34c3b72ec86
Advance <santiwil2010@gmail.com>
2026-08-28 17:17:06 -0300

    Add files via upload
    mejore TODO

---
+++ script.js
@@ -0,0 +1,4 @@
+function bg_image() {
+    const backg = document.querySelector("#header");
+    backg.style.setProperty('url(images/bg/taller_panoramica.jpg)', "url('images/bg/escenario.webp')");
+}
